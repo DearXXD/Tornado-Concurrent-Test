96fe3e25ab5d5d17981e5ece1ce909548733f5be
XiaoXiaoLiu <liu93lyc7@163.com>
2017-09-14 10:13:10 +0800

    Update s.py

--- asynchronous_programming/s.py
+++ asynchronous_programming/s.py
@@ -13,23 +13,25 @@ class hello(tornado.web.RequestHandler):
     def get(self):
         self.write('Hello,xiaoxiao')
 
-class LoginHandler(tornado.web.RequestHandler):
-    executor = ThreadPoolExecutor(2)
+executor = ThreadPoolExecutor(10)
 
-    @tornado.web.asynchronous
+# db = torndb.Connection('127.0.0.1:3306', 'test_t', user='root', password='8782')
+
+def search(db):
+    username = str(random.randint(0, 1000))
+    sql = 'SELECT id FROM user where username=%s and password=123456'
+    cursor = db.get(sql, username)
+    return cursor['id']
+
+class LoginHandler(tornado.web.RequestHandler):
+
+    # @tornado.web.asynchronous
     @tornado.gen.coroutine
     def get(self):
-        res = yield self.search()
-        self.write("success! username %s " % res)
-        self.finish()
 
-    @run_on_executor
-    def search(self):
-        username = str(random.randint(0,1000))
-        sql = 'SELECT id FROM user where username=%s and password=123456' 
-        cursor = self.application.db.get(sql,username)
-        # cursor =   MYSQL_POOL.execute('SELECT * FROM user where username=%s and password=123456' %username)
-        return cursor
+        res = yield  executor.submit( search,self.application.db)
+        self.write("success! username %s " %res )
+        # self.finish()
 
 class CustomApplication(tornado.web.Application):
     def __init__(self):
@@ -44,10 +46,3 @@ if __name__ == "__main__":
     http_server = tornado.httpserver.HTTPServer(CustomApplication())
     http_server.listen(8080)
     tornado.ioloop.IOLoop.instance().start()
-
-
-    # sokets = tornado.netutil.bind_sockets(8080)
-    # tornado.process.fork_processes(0)
-    # http_server = tornado.httpserver.HTTPServer(CustomApplication())
-    # http_server.add_sockets(sokets)
-    # tornado.ioloop.IOLoop.instance().start()
